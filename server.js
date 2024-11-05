3b08acbf9b72b9b2bba6d9137ba518284361338e
oladotun <oladotunadekomaya@gmail.com>
2024-11-05 17:31:12 +0100

    commit

--- server.js
+++ server.js
@@ -6,22 +6,45 @@ const path = require('path')
 
 
 
-// setup static folder
-app.use(express.static(path.join()))
+// setup static folder - where you can create a static file and a route is automatically created with the file name 
+// app.use(express.static(path.join(__dirname, 'public'))) //app.use is for setting up middleware
 
 
+// app.get('/', (req,res) =>{
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    
+// })
+// app.get('/about', (req,res) =>{
+//     res.sendFile(path.join(__dirname, 'public', 'about.html'))
+    
+// })
 
+let posts = [
+    {id: 1, title: 'Post One'},
+    {id: 2, title: 'Post Two'},
+    {id: 3, title: 'Post Three'}
+]
 
-
-app.get('/', (req,res) =>{
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+// Get all posts
+app.get('/api/posts/', (req,res) =>{
+    const limit = parseInt(req.query.limit)
+    if (!isNaN(limit) && limit > 0) {
+        res.json(posts.slice(0,limit))
+    } else {
+        res.json(posts); 
+    }
     
 })
-app.get('/about', (req,res) =>{
-    res.sendFile(path.join(__dirname, 'public', 'about.html'))
-    
-})
 
+// Get a single post
+app.get('/api/posts/:id', (req,res) =>{
+    const id = parseInt(req.params.id)
+    post = posts.find((post) => {
+      return post.id === id
+    })
+    
+    res.json(post);
+})
 
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
